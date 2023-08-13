84e3a560cec04aa13b09651c27382c4f7ae54f7f
Pragyakta singh <singhpragyakta@gmail.com>
2023-08-14 00:12:41 +0530

    added Searchbar, customFilter, carCard with cardetails, custom pagination with showmore button

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { useState, Fragment } from 'react';
+import { Combobox, Transition } from '@headlessui/react'
+import { SearchManufacturerProps } from '@/types'
+import Image from 'next/image';
+
+import { manufacturers } from '@/constants';
+import { isTaggedTemplateExpression } from 'typescript';
+
+
+const SearchManufacturer = ({manufacturer, 
+    setManufacturer}: SearchManufacturerProps) => {
+
+        const [query, setQuery] = useState('')
+
+        const filteredManufacturers = 
+        query === "" ?  //if query was empty return unfiltered data otherwise filtered data
+        manufacturers : manufacturers.filter((item) => (
+            item.toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g,""))  //replace empty spaces with empty string 
+            //if user enters uppercase value or put empty spaces in between
+            //this is going to work in every situation 
+        ))
+  return (
+    <div className='search-manufacturer'>
+        <Combobox value={manufacturer} onChange=
+        {setManufacturer}>
+            <div className='relative w-full'>
+                <Combobox.Button className="absolute top-[14px]">
+                    <Image
+                    src="/car-logo.svg"
+                    width={20}
+                    height={20}
+                    className='ml-4'
+                    alt='Car Logo'
+                    />
+                </Combobox.Button>
+                <Combobox.Input 
+                    className="search-manufacturer__input"
+                    placeholder='volkswagen'
+                    displayValue={(manufacturer:string)=> 
+                    manufacturer}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <Transition
+                 as={Fragment}
+                 leave="transition ease-in duration-100"
+                 leaveFrom="opacity-100"
+                 leaveTo="opacity-0"
+                 afterLeave={() => setQuery('')}
+                 >
+                    <Combobox.Options>
+                        {filteredManufacturers.length === 0 && 
+                        query !== "" ? (
+                            <Combobox.Option 
+                            value={query}
+                            className="search-manufacturer__option">
+                                Create "{query}"
+                            </Combobox.Option>
+                        ): (
+                            filteredManufacturers.map((items) => ( 
+                            <Combobox.Option
+                                key={items}
+                                className={({ active}) => `
+                                relative search-manufacturer__option
+                                ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}
+                                `}
+                                value={items}
+                            >
+                               {({selected,active}) => (
+                                    <>
+                                        <span className={`block truncate ${
+                                            selected ? 'font-medium ' :'font-normal'
+                                        }`}>
+                                            {items}
+                                        </span>
+                                        {selected ? (
+                                            <span
+                                                className={`absolute
+                                                inset-y-0 left-0 flex
+                                                items-center pl-3 ${
+                                                    active ? 'text-white' : 'text-teal-600'
+                                                }`}
+                                                >
+
+                                                </span>
+                                        ) : null}
+                                    </>
+                               )}
+                            </Combobox.Option>
+                            )
+                        ))}
+
+                    </Combobox.Options>
+
+                </Transition>
+            </div>
+        </Combobox>
+    </div>
+  )
+}
+
+export default SearchManufacturer
--- components/index.ts
+++ components/index.ts
@@ -4,6 +4,10 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
 
 export {
    Hero,
@@ -12,4 +16,8 @@ export {
    Footer,
    SearchBar,
    CustomFilter,
+   SearchManufacturer,
+   CarCard,
+   CarDetails,
+   ShowMore,
 }
